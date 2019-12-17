3f9f3b478c0a302d3dee435dbf37c08ffaa950cc
Natsulelou <nutshapon_lelou@hotmail.com>
2019-12-17 11:34:10 +0700

    model

--- src/modules/customerprofile/models/model.js
+++ src/modules/customerprofile/models/model.js
@@ -5,9 +5,62 @@ var Schema = mongoose.Schema;
 
 
 var CustomerprofileSchema = new Schema({
+    frontcardimaged: {
+        type: {
+            url: {
+                type: String
+            }
+        }
+    },
+    backcardimaged: {
+        type: {
+            url: {
+                type: String
+            }
+        }
+    },
+    personwithcardimaged: {
+        type: {
+            url: {
+                type: String
+            }
+        }
+    },
+    citizen_id: {
+        type: String,
+        required: 'Please fill a Customerprofile citizen_id',
+    },
+    citizenback_id: {
+        type: String,
+        required: 'Please fill a Customerprofile citizenback_id',
+    },
     name: {
         type: String,
-        required: 'Please fill a Customerprofile name',
+        required: 'Please fill a Customerprofile name'
+    },
+    lastname: {
+        type: String,
+        required: 'Please fill a Customerprofile lastname',
+    },
+    gender: {
+        type: String,
+        required: 'Please fill a Customerprofile gender',
+    },
+    birthdate: {
+        type: String,
+        required: 'Please fill a Customerprofile birthdate',
+    },
+    addressbycard: {
+        type: String,
+        required: 'Please fill a Customerprofile addressbycard',
+    },
+    email: {
+        type: String,
+        required: 'Please fill a Customerprofile email',
+    },
+    address: {
+        type: String,
+        required: 'Please fill a Customerprofile address',
     },
     created: {
         type: Date,
