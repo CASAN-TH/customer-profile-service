5de46360c58e13c544aac9fbca649585a6bb8938
Natsulelou <nutshapon_lelou@hotmail.com>
2019-12-26 10:35:12 +0700

    create creditpoint

---
+++ src/modules/creditpoint/controllers/controller.js
@@ -0,0 +1,118 @@
+'use strict';
+var mongoose = require('mongoose'),
+    model = require('../models/model'),
+    mq = require('../../core/controllers/rabbitmq'),
+    Creditpoint = mongoose.model('Creditpoint'),
+    errorHandler = require('../../core/controllers/errors.server.controller'),
+    _ = require('lodash');
+
+exports.getList = function (req, res) {
+    var pageNo = parseInt(req.query.pageNo);
+    var size = parseInt(req.query.size);
+    var query = {};
+    if (pageNo < 0 || pageNo === 0) {
+        response = { "error": true, "message": "invalid page number, should start with 1" };
+        return res.json(response);
+    }
+    query.skip = size * (pageNo - 1);
+    query.limit = size;
+        Creditpoint.find({}, {}, query, function (err, datas) {
+            if (err) {
+                return res.status(400).send({
+                    status: 400,
+                    message: errorHandler.getErrorMessage(err)
+                });
+            } else {
+                res.jsonp({
+                    status: 200,
+                    data: datas
+                });
+            };
+        });
+};
+
+exports.create = function (req, res) {
+    var newCreditpoint = new Creditpoint (req.body);
+    newCreditpoint.createby = req.user;
+    newCreditpoint.save(function (err, data) {
+        if (err) {
+            return res.status(400).send({
+                status: 400,
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp({
+                status: 200,
+                data: data
+            });
+            /**
+             * Message Queue
+             */
+            // mq.publish('exchange', 'keymsg', JSON.stringify(newOrder));
+        };
+    });
+};
+
+exports.getByID = function (req, res, next, id) {
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({
+            status: 400,
+            message: 'Id is invalid'
+        });
+    }
+
+    Creditpoint.findById(id, function (err, data) {
+        if (err) {
+            return res.status(400).send({
+                status: 400,
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            req.data = data ? data : {};
+            next();
+        };
+    });
+};
+
+exports.read = function (req, res) {
+    res.jsonp({
+        status: 200,
+        data: req.data ? req.data : []
+    });
+};
+
+exports.update = function (req, res) {
+    var updCreditpoint = _.extend(req.data, req.body);
+    updCreditpoint.updated = new Date();
+    updCreditpoint.updateby = req.user;
+    updCreditpoint.save(function (err, data) {
+        if (err) {
+            return res.status(400).send({
+                status: 400,
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp({
+                status: 200,
+                data: data
+            });
+        };
+    });
+};
+
+exports.delete = function (req, res) {
+    req.data.remove(function (err, data) {
+        if (err) {
+            return res.status(400).send({
+                status: 400,
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp({
+                status: 200,
+                data: data
+            });
+        };
+    });
+};
