9239a4306c227993bca7fa7b25b54114b7730554
Natsulelou <nutshapon_lelou@hotmail.com>
2019-12-26 15:08:24 +0700

    customer Bill model and TDD

--- src/modules/customerbill/models/model.js
+++ src/modules/customerbill/models/model.js
@@ -5,9 +5,71 @@ var Schema = mongoose.Schema;
 
 
 var CustomerbillSchema = new Schema({
-    name: {
-        type: String,
-        required: 'Please fill a Customerbill name',
+    u_id: {
+        type: String
+    },
+    bills: {
+        type: [
+            {
+                bill_month: {
+                    type: String
+                },
+                bill_status: {
+                    type: String
+                },
+                bill_price: {
+                    type: String
+                },
+                bill_datelimit: {
+                    type: String
+                },
+                bill_currency: {
+                    type: String
+                },
+                bill_products: {
+                    type: [
+                        {
+                            product_name: {
+                                type: String
+                            },
+                            product_amount: {
+                                type: String
+                            },
+                            product_currency: {
+                                type: String
+                            },
+                            product_allprice: {
+                                type: String
+                            },
+                            product_charge: {
+                                type: String
+                            },
+                            product_lists: {
+                                type: [
+                                    {
+                                        list_amount: {
+                                            type: String
+                                        },
+                                        list_month: {
+                                            type: String
+                                        },
+                                        list_datelimit: {
+                                            type: String
+                                        },
+                                        list_price: {
+                                            type: String
+                                        },
+                                        list_charge: {
+                                            type: String
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
     },
     created: {
         type: Date,
